a1fa7f28e69aedec987824a3aba048c75a9eb298
Thibaut Jombart <thibautjombart@gmail.com>
2015-02-13 15:20:51 +0000

    passing the check! - 1 note

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -1,2 +1,2 @@
 ## hack to get rid of warnings
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("y","Incidence"))
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("y","Incidence","mydate","freq"))
