22a74cc203aa1ca34af93f389faaa634704a990a
Rolina van Gaalen <rolinavangaalen@hotmail.com>
2015-02-15 11:48:46 +0100

    added ggplot histograms

--- R/clickableTimeSeries.R
+++ R/clickableTimeSeries.R
@@ -10,6 +10,7 @@
 
 library(dygraphs)
 lungDeaths <- cbind(mdeaths, fdeaths)
+dimnames(lungDeaths)[[2]] <- c("male deaths", "female deaths")
 x = dygraph(lungDeaths)
 
 ui = shinyUI(fluidPage(
@@ -40,7 +41,13 @@ server = shinyServer(function(input, output, session){
         date.data <- as.numeric(seq(as.Date("1974-01-01"),as.Date("1979-12-01"), by="month"))
         minRange <- which(abs(date.data-range[1])==min(abs(date.data-range[1])))
         maxRange <- which(abs(date.data-range[2])==min(abs(date.data-range[2])))
-        plot(lungDeaths[minRange:maxRange,], main=toString(input$mydata))
+        data <- melt(lungDeaths, varnames=c("time","deaths"))
+
+        ggplot(data[data$time %in% minRange:maxRange,], aes(x=value)) +
+          facet_wrap(~deaths) +
+          geom_histogram(binwidth=50)
+          #geom_density()
+        #hist(lungDeaths[minRange:maxRange,1], breaks=20, main=paste("Data at ", as.Date(minRange, origin="1970-01-01"))) #toString(input$mydata)))
       } #else plot(1:10, main="")
     )
 })
