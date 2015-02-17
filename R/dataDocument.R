96142acb3a8785fc7e3465955d0158533bfb80f4
hoehleatsu <hoehle@lillagubben>
2015-02-17 20:25:59 +0100

    added dataDocument.R back

---
+++ R/dataDocument.R
@@ -0,0 +1,17 @@
+#' Converted Hagelloch 1861 measles outbreak into obkData format.
+#'
+#' The hagelloch.df data.frame available in the surveillance package
+#' is converted into an object of obkData. Only the individuals slot
+#' of the obkData object is populated. 
+#' 
+#' @name hagelloch.obk
+#' @docType data
+#' @author Michael H\enc{ö}{oe}hle <http://www.math.su.se/~hoehle>
+#' @keywords data
+#' @details See the surveillance package Hagelloch data
+#' \code{\link[surveillance]{hagelloch}} for a description of the
+#' data.  For developers: The conversion is done by a conversion
+#' function \code{hagelloch.obk.R} found in the develop directory of the
+#' package.
+#' 
+"hagelloch.obk"
