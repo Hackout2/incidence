cb3d4003b31a72320ad71620c88c6c0c9350fd78
Thibaut Jombart <thibautjombart@gmail.com>
2015-04-13 14:33:00 +0100

    fixing vignette and dep warning

---
+++ inst/doc/incidence.R
@@ -0,0 +1,5 @@
+## ----setup, echo=FALSE---------------------------------------------------
+# set global chunk options: images will be 7x5 inches
+knitr::opts_chunk$set(fig.width=7, fig.height=10, fig.path="figs/")
+options(digits = 4)
+
